a1ab7f377b7126ee098518d591befb19ca78c413
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-01-29 17:34:11 +0200

    Changed title

---
+++ Dictionaries.py
@@ -0,0 +1,25 @@
+profile_Amy = {
+    "name" : "Amy",
+    "age"  : 18,
+    "power1": "Reality-Manipulation", 
+    "power2" : "Mind-Control",
+    "weakness": "cake",
+}
+
+profile_Amy2 = {
+
+}
+
+profile_Jamal = {
+    "name" : "Jamal",
+    "age"  : 17,
+    "power1": "Geoleaping",
+    "power2": "Memory-Plastering",
+    "weakness": "rain",
+}
+
+profile_Amy.update(profile_Amy2)
+
+print(profile_Amy["name", "age", "power1", "power2", "weakness"])
+print(profile_Amy2["name", "age", "power1", "power2", "weakness"])
+print(profile_Jamal["name", "age", "power1", "power2", "weakness"])
--- Lesson#.py
+++
@@ -1,9 +0,0 @@
-profile = {
-    "name" : "Amy",
-    "age"  : 18,
-    "power1": "Reality-Manipulation", 
-    "power2" : "Mind-Control",
-    "weakness": "cake",
-}
-
-print(profile["name"])
