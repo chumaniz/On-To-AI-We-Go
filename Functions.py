5ba195cc6b9c0a940ad8d47190cb87ca07af33bd
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-01-31 07:36:07 +0200

    Changed function=>statement in ln34

---
+++ Functions.py
@@ -0,0 +1,42 @@
+# This is a function
+
+def hello():
+    print("Hello World!")
+
+hello()
+
+# The purpose of a function is to help create reusable and automatory code...helps with space and repeating oneself
+# Now we add parameters 
+
+def message(string1 , string2):
+    print(string1 + string2)
+  
+message("Hello ", "world!")
+
+def message2(question, name, mark):
+    print(question + name + mark)
+
+message2("Are you going to help out in the kitchen ", "Jason", "?")
+
+# Parameters make our function more faceted, you can do more with it
+# Let's see what we can do mathematically 
+
+def calculation(number1, number2):
+    # For the sake of good practice, I'm going to just add commas simply because you never know...
+    print(number1+number2),
+    print(number1-number2),
+    print(number1/number2),
+    print(number1*number2),
+    print(number1%number2),
+
+calculation(8,4)
+
+# Alright a return statement...used for storing information
+
+def calculation(number1, number2):
+    return(number1+number2)
+
+
+number = calculation(8, 4)
+
+print(calculation(8,4))
