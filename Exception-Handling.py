51c7788a011c1546d9efca9636e76534984486e9
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-02-01 16:11:34 +0200

    Setup of error handling

---
+++ Exception-Handling.py
@@ -0,0 +1,10 @@
+# In python, errors are a normal part of lif
+# Right now we aere finding ways to be able to curb them
+
+math_meme = 10/0 # This will not work as 10 cannot be divided by zero
+
+name = "Zuma"
+
+number = int(name)  # Oh dangerous because a string cannot be turned into an integer
+
+print(number)
