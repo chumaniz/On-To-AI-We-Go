6004262d2f6f857627bfd2176160b219a7aca837
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-03-19 13:21:30 +0200

    More graphs..sort of have a project idea

--- datascience/graph.py
+++ datascience/graph.py
@@ -4,10 +4,23 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 100, 100000)
 y = np.sin(x)
 
-
+# plt.plot(x,y)
+# plt.show()
 
 a = np.linspace(0, 10, 10000)
 b = 4*a+6
 
-plt.plot(a,b)
+# plt.plot(a,b)
+# plt.show()
+
+popul_dens_low = 10*np.random.random(100)
+plt.plot(popul_dens_low, 'bo')
+plt.show()
+
+popul_dens_mid = 10*np.random.random(250)
+plt.plot(popul_dens_low, 'bo')
+plt.show()
+
+popul_dens_high = 10*np.random.random(500)
+plt.plot(popul_dens_high, 'bo')
 plt.show()
