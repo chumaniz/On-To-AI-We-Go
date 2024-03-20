97bc3b3200a0a016c38177f2e2dd4bd0513a07e2
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-03-20 20:43:48 +0200

    Working with datasets for linear regression

---
+++ machinelearning/regression.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# Loading our dataset
+data = pd.read_csv('student-mat.csv', sep=';')
+
+# Specifying exactly what we are looking for
+data = data[['age', 'sex', 'studytime',
+             'absences', 'G1', 'G2', 'G3']]
+
+# Using a dictionary to change sex into a numeric value
+data['sex'] = data['sex'].map({'F':0, 'M':1})
