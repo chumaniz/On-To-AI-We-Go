2b3ef609af40afbcf30af3cf16bb0068da26a461
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-03-21 15:31:18 +0200

    Establishing Visual Relationship between data

--- machinelearning/regression.py
+++ machinelearning/regression.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # Loading our dataset
-data = pd.read_csv('student-mat.csv', sep=';')
+data = pd.read_csv("C:/Users/evepo/OneDrive/Desktop/student/student-mat.csv", sep=';')
 
 # Specifying exactly what we are looking for
 data = data[['age', 'sex', 'studytime',
@@ -15,3 +15,33 @@ data = data[['age', 'sex', 'studytime',
 data['sex'] = data['sex'].map({'F':0, 'M':1})
 
 prediction = 'G3'
+
+X = data.drop(columns=[prediction]).values
+Y = np.array(data[prediction])
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.1)
+
+model = LinearRegression()
+model.fit(X_train, Y_train)
+
+accuracy = model.score(X_test, Y_test)
+
+print(accuracy)
+
+X_new = np.array([[18,1,3,40,15,16]])
+Y_new = model.predict(X_new)
+
+print(Y_new)
+
+plt.scatter(data['studytime'], data['G3'])
+
+plt.title("Correlation")
+plt.xlabel("Study Time")
+plt.ylabel("Final Grade")
+plt.show()
+
+plt.scatter(data['G2'], data['G3'])
+plt.title("Correlation")
+plt.xlabel("Second Grade")
+plt.ylabel("Final Grade")
+plt.show()
